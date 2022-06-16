89d9c090a7ae5a4ac04d7eefb603c1e8bd0acee2
Yogesh <yogeshbietjhs@gmail.com>
2022-06-16 09:17:49 +0530

    inicial commit

---
+++ employeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace employeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random random = new Random();
+            int isEmppresent = random.Next(0, 2);
+            if (isEmppresent == 1)
+            {
+                Console.WriteLine("is present");
+            }
+            else
+            {
+                Console.WriteLine("absent");
+            }
+        }
+    }
+}
