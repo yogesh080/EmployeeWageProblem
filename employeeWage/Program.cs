f13a91714b72e7ca76fc0c677eb424ba1f444a3e
Yogesh <yogeshbietjhs@gmail.com>
2022-06-16 10:53:51 +0530

    employee monthly wage

--- employeeWage/Program.cs
+++ employeeWage/Program.cs
@@ -5,27 +5,38 @@
             public const int partTime = 1;
             public const int fullTime = 2;
             public const int PerHourRate = 20;
+            public const int numOfWorkingday = 2;
+            public const int maxHoursInMonth = 10;
         static void Main(string[] args)
         {
 
             int empHr = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empcheck = random.Next(0,2);
-            switch (empcheck)
+            int totalEmpHrs = 0;
+            int totalWorkingday = 0;
+            while (totalEmpHrs < maxHoursInMonth && numOfWorkingday < totalWorkingday)
             {
-                case partTime:
-                    empHr = 4;
-                    break;
-                case fullTime:
-                    empHr=8;
-                    break;
-                default:
-                    empHr=0;
-                    break;
+                totalWorkingday++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+
+                switch (empcheck)
+                {
+                    case partTime:
+                        empHr = 4;
+                        break;
+                    case fullTime:
+                        empHr=8;
+                        break;
+                    default:
+                        empHr=0;
+                        break;
+                }
+                totalEmpHrs += empHr;
+                Console.WriteLine("day:" + totalWorkingday + "emp hrs:" + empHr);
             }
-            empWage = empHr * PerHourRate;
-            Console.WriteLine(empWage);
+
+            int totalemployeeWage = totalEmpHrs + PerHourRate;
+            Console.WriteLine("total emp wage:" + totalemployeeWage);
 
 
 
